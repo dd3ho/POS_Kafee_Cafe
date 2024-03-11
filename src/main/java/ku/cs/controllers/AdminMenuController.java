9a1d045f11745a241d319ac89b55da5c24cb8033
MKPY <78581568+Punyaphon-Lammapor@users.noreply.github.com>
2024-03-11 21:28:28 +0700

    redirect

--- src/main/java/ku/cs/controllers/AdminMenuController.java
+++ src/main/java/ku/cs/controllers/AdminMenuController.java
@@ -2,14 +2,21 @@ package ku.cs.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import ku.cs.FXRouter;
 
 import java.io.IOException;
 
 public class AdminMenuController {
     @FXML
-    private void handleSignupBtn(ActionEvent event) throws IOException {}
+    private void handleSignupBtn(ActionEvent event) throws IOException {
+        FXRouter.goTo("pos_signup");
+    }
     @FXML
-    private void handleAddMenuBtn(ActionEvent event) throws IOException {}
+    private void handleAddMenuBtn(ActionEvent event) throws IOException {
+        FXRouter.goTo("pos_allMenu");
+    }
     @FXML
-    private void handleAddPromotionBtn(ActionEvent event) throws IOException {}
+    private void handleAddPromotionBtn(ActionEvent event) throws IOException {
+        FXRouter.goTo("pos_promotion");
+    }
 }
--- src/main/java/ku/cs/controllers/StaffMenuController.java
+++ src/main/java/ku/cs/controllers/StaffMenuController.java
@@ -2,14 +2,19 @@ package ku.cs.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import ku.cs.FXRouter;
 
 import java.io.IOException;
 
 public class StaffMenuController {
 
     @FXML
-    private void handleSignupBtn(ActionEvent event) throws IOException {}
+    private void handleSignupBtn(ActionEvent event) throws IOException {
+        FXRouter.goTo("pos_addMember");
+    }
 
     @FXML
-    private void handleAddOrderBtn(ActionEvent event) throws IOException {}
+    private void handleAddOrderBtn(ActionEvent event) throws IOException {
+        FXRouter.goTo("shop");
+    }
 }
