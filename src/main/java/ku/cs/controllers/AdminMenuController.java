8dcd3c766feb8d1f993e2d286aa45d905be410bd
MKPY <78581568+Punyaphon-Lammapor@users.noreply.github.com>
2024-03-11 20:30:51 +0700

    Menu

---
+++ src/main/java/ku/cs/controllers/AdminMenuController.java
@@ -0,0 +1,15 @@
+package ku.cs.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class AdminMenuController {
+    @FXML
+    private void handleSignupBtn(ActionEvent event) throws IOException {}
+    @FXML
+    private void handleAddMenuBtn(ActionEvent event) throws IOException {}
+    @FXML
+    private void handleAddPromotionBtn(ActionEvent event) throws IOException {}
+}
---
+++ src/main/java/ku/cs/controllers/StaffMenuController.java
@@ -0,0 +1,15 @@
+package ku.cs.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class StaffMenuController {
+
+    @FXML
+    private void handleSignupBtn(ActionEvent event) throws IOException {}
+
+    @FXML
+    private void handleAddOrderBtn(ActionEvent event) throws IOException {}
+}
